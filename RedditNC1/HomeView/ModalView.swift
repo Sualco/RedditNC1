bc252f3ab0daef300f91dcc6d5167594f1d6dbcb
Sualco <150017114+Sualco@users.noreply.github.com>
2023-11-21 22:02:21 +0100

    21/11 evening

---
+++ RedditNC1/HomeView/ModalView.swift
@@ -0,0 +1,18 @@
+//
+//  ModalView.swift
+//  RedditNC1
+//
+//  Created by Claudio Borrelli on 21/11/23.
+//
+
+import SwiftUI
+
+struct ModalView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ModalView()
+}
---
+++ RedditNC1/HomeView/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  RedditNC1
+//
+//  Created by Claudio Borrelli on 21/11/23.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
